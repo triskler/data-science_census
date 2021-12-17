51f50eee28467e4d2f518a4ae1d457ba889d2e0b
RafaelFonseca <fonseca.wutang@gmail.com>
2021-12-17 14:41:17 +0000

    including birth year data

--- script.py
+++ script.py
@@ -13,17 +13,35 @@ census['birth_year'] = census['birth_year'].astype(int)
 # add column 'higher tax' for new variable for machine learning in next step
 census_values = pd.get_dummies(data=census, columns=['higher_tax'])
 
-mean_income = census['income_year'].mean()
-print(mean_income)
-median_income = census['income_year'].median()
-print(median_income)
+print('-------Column Income_year data-------')
+print('' * 20)
+# mean data for income for year
+mean_income = round(census['income_year'].mean(), 2)
+print(f' *  The average annual income is {mean_income}')
+# median
+median_income = round(census['income_year'].median(), 2)
+print(f' *  The median plotted for the annual income is {median_income}')
+# variance
+variance_income = round(census['income_year'].var(), 2)
+print(f' *  The variance for the annual income is in {variance_income}')
+# standard deviation
+std_income = round(census['income_year'].std(), 2)
+print(f' *  The standard deviation of annual income is {std_income}')
 
+print('' * 20)
+print('' * 20)
 
+print('-------Column birth_year data --------')
+print('' * 20)
 
+mean_birth = int(census['birth_year'].mean())
+print(f' *  The average of the year of birth is {mean_birth}')
 
-#print(census.dtypes)
-#ensus.birth_year = census.birth_year.replace(['missing'], 1967)
-#print(census.birth_year.unique())
+median_birth = int(census['birth_year'].median())
+print(f' *  The median of the year of birth is {median_birth}')
 
-#
-#print(census.head())
+variance_birth = int(census['birth_year'].var())
+print(f' *  The variance of the year of birth is {variance_birth}')
+
+std_birth = int(census['birth_year'].std())
+print(f' *  The standard deviation of the year of birth is {std_birth}')
